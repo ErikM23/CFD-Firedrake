cda2800777041579ed5f714f8c2622d99ad0c64c
ErikM23 <67972140+ErikM23@users.noreply.github.com>
2020-08-17 15:38:11 +0200

    update

--- Lid_driven_cavity/unsteady/2D/FPCD.py
+++ Lid_driven_cavity/unsteady/2D/FPCD.py
@@ -13,7 +13,6 @@ class PCD(PCBase):
         from firedrake import TrialFunction, TestFunction, Function, DirichletBC, dx, \
              assemble, Mesh, inner, grad, split, Constant, parameters
         from firedrake.assemble import allocate_matrix, create_assembly_callable
-        #from assamble1 import allocate_matrix, apply_bcs, create_assembly_callable
 
         prefix = pc.getOptionsPrefix() + "pcd_"
 
@@ -30,16 +29,11 @@ class PCD(PCBase):
         nu = context.appctx["nu"]
         dt = context.appctx["dt"]
 
-        # pressure mass matri
         mass = (1.0/nu)*p*q*dx
-        # stiffness matrix
+        
         stiffness = inner(grad(p), grad(q))*dx
         
         velid = context.appctx["velocity_space"]
-        #preid = context.appctx["pressure_space"]
-
-        #self.bcs = context.appctx["PCDbc"]
-        #self.bcs = DirichletBC(Q,0,2)
 
         opts = PETSc.Options()
         
@@ -73,9 +67,6 @@ class PCD(PCBase):
         Kksp.setUp()
         Kksp.setFromOptions()
         self.Kksp = Kksp
-
-        #state = context.appctx["state"]
-        #velid = context.appctx["velocity_space"]
         
         u=context.appctx["u"]
         fp = (1.0/nu)*((1.0/dt)*p + dot(u,grad(p)))*q*dx
@@ -91,32 +82,25 @@ class PCD(PCBase):
         self.Fp=Fp
         self._assemble_Fp()
         self.workspace = [Fp.petscmat.createVecLeft() for i in (0, 1)]
-        #self.tmp = Function(Q)
     
     def update(self, pc):
-        #print("PC UPDATE")
         self._assemble_Fp()
 
 
     def apply(self, pc, x, y):
         a, b = self.workspace
+        
         self.Mksp.solve(x, y)
         y.copy(result=a)
+        
         self.Fp.petscmat.mult(a, b)
 
-        #with self.tmp.dat.vec_wo as v:
-        #    b.copy(v)
-        # Now tmp contains the value from `b`
-        #self.bcs.apply(self.tmp)
-        #with self.tmp.dat.vec_ro as v:
-        #    v.copy(b)
-
         self.Kksp.solve(b, a)
+        
         y.axpy(1.0,a)
         y.scale(-1.0)
 
     def applyTranspose(self, pc, x, y):
-        print("TRANSPOSE")
         pass
 
     def view(self, pc, viewer=None):
