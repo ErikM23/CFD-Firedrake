1c79d91439bccece44b87907bb65b76a6620acb5
ErikM23 <67972140+ErikM23@users.noreply.github.com>
2020-08-17 15:44:30 +0200

    update

--- Lid_driven_cavity/unsteady/3D/uNS_3D.py
+++ Lid_driven_cavity/unsteady/3D/uNS_3D.py
@@ -1,7 +1,5 @@
 from firedrake import *
 from FPCD import*
-from PCD import*
-from FPCD2 import*
 
 print = PETSc.Sys.Print
 
@@ -33,21 +31,18 @@ def F_(u,p,v,q) :
 
 F = Constant(1.0/dt)*inner((u-u0),v)*dx + theta* F_(u,p,v,q) +(1-theta)*F_(u0,p0,v,q)
 
-
 V = Constant(1)
 
 bcs = [DirichletBC(Z.sub(0), Constant((V, 0, 0)), 4), DirichletBC(Z.sub(0), Constant((0, 0, 0)), (1, 2, 3, 5, 6))]
 
 nullspace = MixedVectorSpaceBasis(Z, [Z.sub(0), VectorSpaceBasis(constant=True)])
 
-#appctx = {"Re": Re, "velocity_space": 0}
 appctx = {"nu": nu, "velocity_space": 0}
 
-parLSC = {
+LSC = {
         "ksp_type": "fgmres",
         "ksp_monitor":None,
-        #"ksp_max_it":100000,
-        "ksp_gmres_restart":400,#200#600#500
+        "ksp_gmres_restart":400,
         "ksp_rtol":1e-3,
         "ksp_max_it":400,
         "snes_monitor":None,
@@ -58,83 +53,70 @@ parLSC = {
         "pc_fieldsplit_schur_fact_type":"upper",
         "fieldsplit_0":{
             "ksp_type":"gmres",
-            #"ksp_max_it":2,
-            "ksp_gmres_restart":50, #50
-            #"ksp_monitor":None,
+            "ksp_gmres_restart":50,
             "pc_type":"bjacobi",
                         },
         "fieldsplit_1":{
-            "ksp_type":"preonly",#gmres
+            "ksp_type":"preonly",
             "pc_type":"lsc",
-            #"pc_lsc_scale_diag":None,
             "lsc_pc_type":"ksp",
-            #"lsc_pc_factor_mat_solver_type":"mumps",
-            #"lsc_pc_sub_type":"ilu",
-            "lsc_pc_ksp_type":"gmres",#gmres
+            "lsc_pc_ksp_type":"gmres",
             "lsc_pc_ksp_rtol":1e-2,
             "lsc_pc_ksp_max_it":50,
             "lsc_pc_ksp_gmres_restart":50,
-            "lsc_pc_pc_type":"hypre",#bjacobi
+            "lsc_pc_pc_type":"hypre",
             "lsc_pc_pc_hypre_type":"boomeramg",  
             "lsc_pc_pc_hypre_boomeramg_strong_threshold": 0.2,       
             "lsc_pc_pc_hypre_boomeramg_P_max":2,
-            #"lsc_pc_ksp_rtol":1e-4,
                         }
         }
 
 
-parDIR={"snes_monitor": None,
+MUMPS = {
+        "snes_monitor": None,
         "snes_max_it":1000,
         "ksp_type": "preonly",
         "mat_type": "aij",
         "pc_type": "lu",
-        "pc_factor_mat_solver_type": "mumps"}
+        "pc_factor_mat_solver_type": "mumps"
+            }
 
-parPCD = {
+PCD = {
         "mat_type": "matfree",
         "snes_monitor": None,
         "ksp_monitor":None,
         "ksp_type": "fgmres",
-        "ksp_rtol": 1e-2,
-        #"snes_rtol":1e-5,
+        "ksp_rtol": 1e-3,
         "ksp_gmres_restart": 600,
+        "ksp_max_it":800,
         "ksp_gmres_modifiedgramschmidt": None,
-        #"ksp_monitor_true_residual": None,
         "pc_type": "fieldsplit",
         "pc_fieldsplit_type": "schur",
         "pc_fieldsplit_schur_fact_type": "upper",
 
-        "fieldsplit_0_ksp_type": "preonly",
-        #"fieldsplit_0_ksp_gmres_restart": 50,
-        #"fieldsplit_0_ksp_max_it":2,
+        "fieldsplit_0_ksp_type": "gmres",
+        "fieldsplit_0_ksp_gmres_restart": 100,
+        "fieldsplit_0_ksp_max_it":10,
         "fieldsplit_0_pc_type": "python",
         "fieldsplit_0_pc_python_type": "firedrake.AssembledPC",
-        "fieldsplit_0_assembled_pc_type": "hypre",
-        #"fieldsplit_0_assembled_pc_sub_type": "ilu",
-        "fieldsplit_0_assembled_pc_hypre_boomeramg_max_iter": 2,
-        #"fieldsplit_0_assembled_pc_factor_mat_solver_type":"mumps",
-        #"fieldsplit_0_assembled_pc_sub_type": "ilu",
+        "fieldsplit_0_assembled_pc_type": "bjacobi",
 
-        "fieldsplit_1_ksp_type": "preonly",
-        #"fieldsplit_1_ksp_rtol": 1e-4,
+        "fieldsplit_1_ksp_type": "gmres",
+        "fieldsplit_1_ksp_rtol": 1e-1,
+        "fieldsplit_1_ksp_max_it": 10,
         "fieldsplit_1_pc_type": "python",
-        "fieldsplit_1_pc_python_type": "FPCD2.PCD",
+        "fieldsplit_1_pc_python_type": "FPCD.PCD",
 
         "fieldsplit_1_pcd_Mp_ksp_type": "richardson",
-        "fieldsplit_1_pcd_Mp_ksp_max_it": 3,
+        "fieldsplit_1_pcd_Mp_ksp_max_it": 5,
         "fieldsplit_1_pcd_Mp_pc_type": "sor",
-        #"fieldsplit_1_pcd_Mp_pc_factor_mat_solver_type": "mumps",
+
 
         "fieldsplit_1_pcd_Kp_ksp_type": "preonly",
-        #"fieldsplit_1_pcd_Kp_ksp_max_it": 1,
-        "fieldsplit_1_pcd_Kp_pc_type": "telescope",
-        #"fieldsplit_1_pcd_Kp_pc_telescope_reduction_factor": "6",
-        "fieldsplit_1_pcd_Kp_pc_telescope_pc_type": "ksp",
-        "fieldsplit_1_pcd_Kp_pc_telescope_ksp_ksp_type": "richardson",
-        "fieldsplit_1_pcd_Kp_pc_telescope_ksp_ksp_max_it": 3,
-        "fieldsplit_1_pcd_Kp_pc_telescope_ksp_pc_type": "hypre",
-
-        #"fieldsplit_1_pcd_Kp_pc_factor_mat_solver_type": "mumps",
+        "fieldsplit_1_pcd_Kp_pc_type": "hypre",
+        "fieldsplit_1_pcd_Kp_pc_hypre_type":"boomeramg",
+        "fieldsplit_1_pcd_Kp_pc_hypre_boomeramg_strong_threshold": 0.2,
+        "fieldsplit_1_pcd_Kp_pc_hypre_boomeramg_P_max":2,
 
         "fieldsplit_1_pcd_Fp_mat_type": "matfree"
         }
@@ -156,17 +138,17 @@ def create_solver(solver_parameters, appctx):
     return solver
 
 
-solver = create_solver(solver_parameters=parLSC, appctx=appctx)
+solver = create_solver(solver_parameters=LSC, appctx=appctx)
 
-outfile = File("NS3D_cavity_time.pvd")
+#outfile = File("NS3D_cavity_time.pvd")
 
 t = 0.0
 
 w.assign(0)
 w0.assign(w)
-u0, p0 = w0.split()
-u0.rename("Velocity", "Velocity")
-p0.rename("Pressure", "Pressure")
+#u0, p0 = w0.split()
+#u0.rename("Velocity", "Velocity")
+#p0.rename("Pressure", "Pressure")
 
 print("Solving ...")
 
@@ -187,6 +169,6 @@ while t<t_end:
 #p.rename("Pressure")
 
 #File("cavity.pvd").write(u, p)
-print(w.function_space().mesh().num_cells())
-convergence(solver)
+#print(w.function_space().mesh().num_cells())
+#convergence(solver)
 
